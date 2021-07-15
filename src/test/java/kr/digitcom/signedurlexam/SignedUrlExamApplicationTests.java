e5fd90345f5e61e0daa2546821652e7e053519c2
younghoon <digitcomgisul@gmail.com>
2021-07-15 18:25:12 +0900

    Initial commit

---
+++ src/test/java/kr/digitcom/signedurlexam/SignedUrlExamApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.digitcom.signedurlexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignedUrlExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
